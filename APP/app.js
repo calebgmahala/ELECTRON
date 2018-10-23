af0eab61646b0a389579ccbf4ee5f8ff5039a576
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-10-23 09:01:42 -0400

    wip. working on base template style

--- APP/app.js
+++ APP/app.js
@@ -1,14 +1,15 @@
 // express is main framework
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
 const port = 3000
 
 // require is used to send out api requests
 const request = require('request')
 
 // mustache is template handler
-var mustache = require('mustache-express');
-app.engine('html', mustache());
+var mustache = require('mustache-express')
+app.engine('html', mustache())
 app.set('view engine', 'html')
 app.set('views', __dirname + '/views')
 
@@ -25,8 +26,23 @@ function CallApi(a, b) {
 
 // show all leagues
 app.get('/leagues', function(req, res) {
+	// function to put together table for html page
+	function Table(a) {
+		a = JSON.parse(a)// turn a into object otherwise it is a string
+		string = "<tr><th>Id</th><th>Name</th><th>Owner</th><th>Entry Fee</th><th>Description</th></tr>";
+		for (var b in a) {
+			string = string + "<tr><td>"
+			string = string + a[b]['id'] + "</td><td>"
+			string = string + a[b]['name'] + "</td><td>"
+			string = string + a[b]['owner_id'] + "</td><td>"
+			string = string + a[b]['entry_fee'] + "</td><td>"
+			string = string + a[b]['desc'] + "</td></tr>"
+		}
+		return(string)
+	}
+	// make an api call and on response render the html page.
 	CallApi('leagues', function(body){
-		res.render('leagues.html', {"body": body})
+		res.render('leagues.html', {"table": Table(body), "title": "Leagues", "user": "Placeholder", "body": body})
 	})
 })
 
