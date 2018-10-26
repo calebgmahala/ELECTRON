3191127667fcb2bf8559671beb60411ab49c7827
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-10-26 16:01:13 -0400

    working on testing

--- API/api/app.py
+++ API/api/app.py
@@ -8,17 +8,27 @@ import simplejson as json
 #used to decode and encode json
 import os
 
-#connect to mysql database
 app = Flask(__name__)
 cors = CORS(app)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'csx'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql = MySQL()
-mysql.init_app(app)
 
-#set connection and cursor variables
-conn =  mysql.connect()
+#connect to mysql database
+def connect_db(params):
+	app.config['MYSQL_DATABASE_USER'] = 'root'
+	#set up testing db or production db
+
+	if (params == 'testing'):
+		app.config['MYSQL_DATABASE_DB'] = 'testing'
+	elif (env == 'production'):
+		app.config['MYSQL_DATABASE_DB'] = 'csx'
+		
+	app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+	mysql = MySQL()
+	mysql.init_app(app)
+	return mysql.connect()
+
+
+#set connection and cursor variables\
+conn = connect_db('production')
 cur = conn.cursor()
 
 #league routes
@@ -37,7 +47,6 @@ def leagues():
 		body = json.dumps(request.form)
 		body = json.loads(body)
 		body['entry_fee'] = int(body['entry_fee'])
-		print(body)
 		cur.execute('INSERT INTO organizers (name, entry_fee, owner_id, description) VALUES(%(name)s, %(entry_fee)s, 1, %(description)s)', body)
 		conn.commit()
 		return redirect('localhost:5000/leagues', code=200)
@@ -49,17 +58,17 @@ def showleague(id):
 		cur.execute("SELECT * FROM organizers WHERE id=%d" % id)
 		org = cur.fetchone()
 		money = str(org[3])[:-2] + "." + str(org[3])[-2:] #change entry fee from cent int to string decimal (used to display in json)
-		resp = {"id":org[0], "name":org[1], "owner_id":org[2], "entry_fee":money, "desc":org[4]}
+		resp = {"id":org[0], "name":org[1], "owner_id":org[2], "entry_fee":money, "description":org[4]}
 		return json.dumps(resp)
 	elif request.method == 'PUT':
 		body = json.dumps(request.form)
 		body = json.loads(body)
 		body['entry_fee'] = int(body['entry_fee'])
 		body['id'] = id
-		print(body)
 		cur.execute('UPDATE organizers SET name = %(name)s, entry_fee = %(entry_fee)s, owner_id = 1, description = %(description)s WHERE id=%(id)s', body)
 		conn.commit()
-		return redirect('localhost:5000/leagues', code=200)
+		return redirect('localhost:5000/leagues/'+id, code=200)
 	elif request.method == 'DELETE':
 		cur.execute('DELETE FROM organizers WHERE id=%d' % id )
 		conn.commit()
+		return redirect('localhost:5000/leagues', code=200)
